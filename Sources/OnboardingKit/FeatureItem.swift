9e7e2cc4ca012fe8168db04dbde51d0825f28485
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2025-11-28 16:17:03 -0500

    allow for system images to have their own colors

--- Sources/OnboardingKit/FeatureItem.swift
+++ Sources/OnboardingKit/FeatureItem.swift
@@ -13,18 +13,21 @@ public struct FeatureItem: Identifiable, Equatable {
     public let description: String
     public let icon: OnboardingIcon
     public let backgroundColor: Color // New Property
+    public let iconColor: Color?
     
-    public init(title: String, description: String, systemImage: String, backgroundColor: Color = .clear) {
+    public init(title: String, description: String, systemImage: String, backgroundColor: Color = .clear, iconColor: Color? = nil) {
         self.title = title
         self.description = description
         self.icon = .system(systemImage)
         self.backgroundColor = backgroundColor
+        self.iconColor = iconColor
     }
     
-    public init(title: String, description: String, image: String, backgroundColor: Color = .clear) {
+    public init(title: String, description: String, image: String, backgroundColor: Color = .clear, iconColor: Color? = nil) {
         self.title = title
         self.description = description
         self.icon = .asset(image)
         self.backgroundColor = backgroundColor
+        self.iconColor = iconColor
     }
 }
--- Sources/OnboardingKit/OnboardingImageView.swift
+++ Sources/OnboardingKit/OnboardingImageView.swift
@@ -10,16 +10,25 @@ import SwiftUI
 struct OnboardingImageView: View {
     let icon: OnboardingIcon
     let tintColor: Color
+    let symbolColor: Color?
     let size: CGFloat
     
+    init(icon: OnboardingIcon, tintColor: Color, symbolColor: Color? = nil, size: CGFloat) {
+        self.icon = icon
+        self.tintColor = tintColor
+        self.symbolColor = symbolColor
+        self.size = size
+    }
+    
     var body: some View {
+        let effectiveColor = symbolColor ?? tintColor
         switch icon {
             case .system(let name):
                 Image(systemName: name)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: size, height: size)
-                    .foregroundStyle(tintColor)
+                    .foregroundStyle(effectiveColor)
             case .asset(let name):
                 Image(name)
                     .resizable()
