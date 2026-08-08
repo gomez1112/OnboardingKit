9f52520bd3fb332a798b80e899aa5f6658de9d29
gomez1112 <54489633+gomez1112@users.noreply.github.com>
2026-08-08 14:42:08 -0400

    Unify onboarding around OnboardingFlow

--- Sources/OnboardingKit/OnboardingCopy.swift
+++ Sources/OnboardingKit/OnboardingCopy.swift
@@ -1,26 +1,18 @@
-import SwiftUI
-
 /// User-facing copy used by onboarding views.
 public struct OnboardingCopy: Sendable, Equatable {
     public var skipButtonTitle: String
     public var nextButtonTitle: String
     public var getStartedButtonTitle: String
-    public var continueButtonTitle: String
-    public var whatsNewHeaderTitle: String
 
     public static let `default` = OnboardingCopy()
 
     public init(
         skipButtonTitle: String = "Skip",
         nextButtonTitle: String = "Next",
-        getStartedButtonTitle: String = "Get Started",
-        continueButtonTitle: String = "Continue",
-        whatsNewHeaderTitle: String = "What's New in"
+        getStartedButtonTitle: String = "Get Started"
     ) {
         self.skipButtonTitle = skipButtonTitle
         self.nextButtonTitle = nextButtonTitle
         self.getStartedButtonTitle = getStartedButtonTitle
-        self.continueButtonTitle = continueButtonTitle
-        self.whatsNewHeaderTitle = whatsNewHeaderTitle
     }
 }
